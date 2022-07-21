313e0f8203fb1b040672644ba501ba2c3365c748
clarkb21 <bmclark7694@gmail.com>
2022-07-21 17:22:58 -0400

    Finalizing Challenge files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -53,4 +53,34 @@ FROM employees as e
 WHERE (de.to_date = '9999-01-01')
 AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY emp_no;
+
+-- How many roles need to be filled?
+SELECT COUNT(title) FROM unique_titles;
+
+-- Number of mentors per department
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	d.dept_name
+INTO mentorship_departments
+FROM employees as e
+	INNER JOIN dept_employees as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN title as t
+		ON (e.emp_no = t.emp_no)
+	INNER JOIN departments as d
+		ON (d.dept_no = de.dept_no)
+WHERE (de.to_date = '9999-01-01')
+AND (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+GROUP BY de.dept_no, e.emp_no, t.title, d.dept_name
+ORDER BY emp_no;
+
+SELECT COUNT(dept_name),
+	dept_name,
+	title
+INTO mentor_counts
+FROM mentorship_departments
+GROUP BY dept_name, title
+ORDER BY dept_name;
 	
